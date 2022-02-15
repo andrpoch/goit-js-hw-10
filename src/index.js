10874a46096db5aef646b94b6d6b052443b615fa
andrpoch <andrpoch25@gmail.com>
2022-02-15 17:14:37 +0200

    Complete tast

--- src/index.js
+++ src/index.js
@@ -3,35 +3,68 @@ import Notiflix from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './services/fetchCountries'
 
+const DEBOUNCE_DELAY = 300;
+
 const refs = {
   inputRef: document.querySelector('#search-box'),
   listRef: document.querySelector('.country-list'),
   infoRef: document.querySelector('.country-info'),
 };
 
-const DEBOUNCE_DELAY = 300;
-
-const findCountries = e => {
-   e.preventDefault();
-   const form = e.currentTarget;
-   const country = form.elements.country.value.trim();
-   console.log(form);
-      console.log(country);
+const findCountries = (e) => {
+   const country = e.target.value.trim();
    if (!country) {
       Notiflix.Notify.warning('Enter country');
+      clearInfo();
       return;
    }
    fetchCountries(country)
-      .then(countries => renderCountry(countries))
-      .catch(err => console.log(err));
+      .then(countries => {
+         clearInfo();
+         if (countries.length === 1) {
+            renderInfo(countries);
+            return;
+         }else if (10 < countries.length > 1) {
+            renderCountry(countries);
+         } else if(countries.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+         } else {
+            renderCountry(countries);
+         }
+      })
+      .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
 };
 
-const renderCountry = countries => {
-   const markup = createMarkup(countries);
-   refs.listRef.innerHTML = markup;
+const renderCountry = arr => {
+   const markup = arr.map(({ name, flags }) => {
+      return `<li class="country__item">
+      <img src="${flags.svg}" alt="${name.common}" width="30" height="30"><h2 class="country__name">${name.official}</h2></li>`;
+   }).join('');
+   refs.listRef.insertAdjacentHTML('afterbegin', markup);
 }
-const createMarkup = ({ name }) =>
-   `<li>${name.offical}</li>`;
+ 
+const renderInfo = arr => {
+   const markup = arr.map(({
+      name, capital, population, languages, flags
+   }) => {
+      return `<div class="country__wrapper">
+      <img src="${flags.svg}" alt="${name.common}" width = "30" height="30">
+      <h2>${name.official}</h2>
+      <p>
+      <span>Capital:</span>${capital}</p>
+      <p><span>Population:</span>${population}</p><p><span>Languages:</span>${Object.values(languages).join(', ')}</p></div>`
+   })
+      .join('');
+   refs.infoRef.insertAdjacentHTML('afterbegin', markup);
+}
+const clearInfo = () => {
+   refs.listRef.innerHTML = '';
+   refs.infoRef.innerHTML = '';
+}
 
 
 refs.inputRef.addEventListener('input', debounce(findCountries, 300));
+
+
+
+
--- src/services/fetchCountries.js
+++ src/services/fetchCountries.js
@@ -1,6 +1,14 @@
-const COUNTRY_URL = `https://restcountries.com/v3.1`;
+const COUNTRY_URL = `https://restcountries.com/v3.1/name/`;
+
+const fieldsNames = `?fields=name,capital,population,flags,languages`;
+
 const fetchCountries = (name) => {
-   return fetch(`${COUNTRY_URL}/name/${name}`).then(response =>response.json()) 
+   return fetch(`${COUNTRY_URL}${name}${fieldsNames}`).then(response => {
+      if (!response.ok) {
+         throw new Error(response.message);
+      }
+      return response.json();
+   }) 
 };
 
 
